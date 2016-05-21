7e02c21a4a7f0d5e6f76319de1fa2bba615c3705
cs <cs@Sawasdithep>
2016-05-22 02:38:48 +0700

    New data store model push

---
+++ ATMErrakError/ATM_TK/src/model/DataStore.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author cs
+ */
+public class DataStore {
+    static String id, password, name, balance;
+    static String no;
+    static String no_each;
+    static int cnt = 1;
+    static String mode, amount;
+    
+    //Collect Data for get and set
+
+    public static void setInfo(String id, String password, String name, String balance){
+        DataStore.id = id;
+        DataStore.password = password;
+        DataStore.name = name;
+        DataStore.balance = balance;
+    }
+    public static void setCurrentBalance(String balance){
+        DataStore.balance = balance;
+    }
+    
+    public static void setStateMe(String no) {
+        DataStore.no = no;   
+    }
+
+    public static void setStateMe_Each(String no_each) {
+        DataStore.no_each = no_each;   
+    }
+    
+    public static void setModeAmount(String mode, String amount) {
+        DataStore.mode = mode;
+        DataStore.amount = amount;   
+    }
+    public static String getCustomerID(){
+        return id;   
+    }
+    public static String getCurrentBalance(){
+        return balance;   
+    }
+    public static String getName(){
+        return name;   
+    }
+
+}
