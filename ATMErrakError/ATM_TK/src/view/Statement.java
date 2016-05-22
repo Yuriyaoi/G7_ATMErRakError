1592fe8976293ba2b7e0bf3350e14bf51649eb73
Tanyaluk <tanya-luk3127@hotmail.com>
2016-05-22 07:19:39 +0700

    add statement table

--- ATMErrakError/ATM_TK/src/view/Statement.java
+++ ATMErrakError/ATM_TK/src/view/Statement.java
@@ -11,6 +11,7 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
 
 public class Statement extends JFrame{
     
@@ -18,57 +19,37 @@ public class Statement extends JFrame{
 	private JButton back;
         private JLabel background;
         private JTable table;
+        private JScrollPane scroll;
 	public Statement() 
-        {
-                
+        {         
 		panel = new JPanel();
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panel.setLayout(null);
+		panel.setLayout(null);	     
+                
+                scroll = new JScrollPane();
+                scroll.setBounds(130,150,550,350);
+                scroll.setFont(new Font("Lucida Sans", Font.PLAIN, 16));
+                scroll.setBackground(new Color(65, 171, 225));
+                scroll.setBorder(null);
+                
+                table = new JTable();
+                table.setFont(new Font("Lucida Sans", Font.PLAIN, 16));
+                table.setBorder(null);
+                table.setBackground(new Color(65, 171, 225));
+                table.setModel(new DefaultTableModel
+                (
+                    new Object [][] {}, 
+                    new String [] {"Transaction NO", "Customer ID", "Mode", "Amount"}
+                ));
+                
+                scroll.setViewportView(table);
+                panel.add(scroll);
 
-//		table.setModel(new DefaultTableModel
-//                (
-//                    new Object [][] {},
-//                    new String [] {"No.", "ID", "Mode", "Amount"}
-//                ) 
-//                {
-//                    boolean[] canEdit = new boolean [] {false, false, false, false};
-//
-//                    public boolean isCellEditable(int rowIndex, int columnIndex) 
-//                    {
-//                        return canEdit [columnIndex];
-//                    }
-//                });
-//                panel.add(table);
-            
-//                jScrollPane1.setViewportView(User_Statement);
-//                if (User_Statement.getColumnModel().getColumnCount() > 0) {
-//                User_Statement.getColumnModel().getColumn(0).setResizable(false);
-//                User_Statement.getColumnModel().getColumn(0).setPreferredWidth(10);
-//                User_Statement.getColumnModel().getColumn(1).setResizable(false);
-//                User_Statement.getColumnModel().getColumn(1).setPreferredWidth(100);
-//                User_Statement.getColumnModel().getColumn(2).setResizable(false);
-//                User_Statement.getColumnModel().getColumn(3).setResizable(false);
-//                }
-		setContentPane(panel);
-		
-//		bill_ID = new JTextField();
-//		bill_ID.setBounds(275, 202, 296, 33);
-//                bill_ID.setBorder(null);
-//		panel.add(bill_ID);
-//		bill_ID.setColumns(10);
-//		
-//		amount = new JTextField();
-//		amount.setColumns(10);
-//                amount.setBorder(null);
-//		amount.setBounds(275, 311, 296, 33);
-//		panel.add(amount);
-		
-		
 		back = new JButton("Back");
 		back.setBounds(15, 535, 109, 33);
 		back.setBackground(new Color(85, 85, 85));
 		back.setForeground(Color.WHITE);
-		back.setFocusPainted(false); //กรอบตัวอักษร
+		back.setFocusPainted(false); 
 		back.setFont(new Font("Yu Gothic UI", Font.PLAIN, 20));
 		panel.add(back);
 		
@@ -77,7 +58,7 @@ public class Statement extends JFrame{
 		background.setBounds(0, 0, 800, 600);
 		panel.add(background);
 		
-
+                setContentPane(panel);
                 setBounds(100, 100, 800, 600);
                 setUndecorated(true);
                 setLocationRelativeTo(null);
@@ -86,20 +67,5 @@ public class Statement extends JFrame{
         public void setActionBack(ActionListener act)
         {
             back.addActionListener(act);
-        }
-        private void User_StatementAncestorAdded(javax.swing.event.AncestorEvent evt) {                                             
-//            DefaultTableModel model = (DefaultTableModel) User_Statement.getModel();
-//            int c = 0;
-//            storeData.no_each = "0";
-//            for(int i = 0; i < storeData.cnt ; i++) {
-//              getDB.statementStoreNo_Each();
-//              c++;
-//            }
-//            storeData.no_each = "0";
-//            for(int i = 1; i < c ; i++) {
-//                getDB.statementStoreNo_Each();
-//                model.addRow(new Object[]{i,storeData.user,storeData.mode,storeData.amount});
-//            }
-//            storeData.cnt = 1;
-        }                                             
+        }                              
 }
